53783411f8bbcea3c27a471acb17d1215148802d
Izan Wang <izanwang377@gmail.com>
2024-09-29 10:03:03 -0400

    ccc20j3 change

--- 2020/ccc20j3.py
+++ 2020/ccc20j3.py
@@ -1,20 +1,15 @@
-N = int(input())
-
-min_x = float('inf')
-min_y = float('inf')
-max_x = float('-inf')
-max_y = float('-inf')
-
-for _ in range(N):
-    x, y = map(int, input().split(','))
-    if x < min_x:
-        min_x = x
-    if y < min_y:
-        min_y = y
-    if x > max_x:
-        max_x = x
-    if y > max_y:
-        max_y = y
-
-print(f"{min_x-1},{min_y-1}")
-print(f"{max_x+1},{max_y+1}")
+n = int(input())
+list_x = []
+list_y = []
+# for i in range(n):
+#     x,y = map(int, input().split(","))
+#     list_x.append(x)
+#     list_y.append(y)
+for i in range(n):
+    k = input().split(",")
+    list_x.append(int(k[0]))
+    list_y.append(int(k[1]))
+print(list_x)
+print(list_y)    
+print(f"{min(list_x)-1},{min(list_y)-1}")
+print(f"{max(list_x)+1},{max(list_y)+1}")
